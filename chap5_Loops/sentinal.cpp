eace0d92eeadbe7ab7979e3d6a5d1393ec834bb7
Benjamin Mendosa <benjaminmendosa1202@gmail.com>
2025-09-28 00:01:11 -0700

    added a short program using a sentinal

---
+++ chap5_Loops/sentinal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string userResponse;
+    while(userResponse != "END" || userResponse != "end"){
+        cin >> userResponse;
+    }
+}
